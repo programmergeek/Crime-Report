6360c1c19b340211055a7b37607f6dfbb92a544a
Will <programergeek@gmail.com>
2022-05-05 21:49:43 +0200

    create project structure

---
+++ config/config.php
@@ -0,0 +1,11 @@
+<?php
+
+$config = [
+    'database' => [
+        'hostname' => 'csi233.mysql.database.azure.com',
+        'port' => 3306,
+        'name' => 'csi233',
+        'username' => 'database_admin',
+        'password' => 'pass_word1'
+    ]
+];
---
+++ public/index.php
@@ -0,0 +1,5 @@
+<?php
+
+if ($_SERVER['REQUEST_URI'] === '/') {
+    include_once "../templates/Index.php";
+}
