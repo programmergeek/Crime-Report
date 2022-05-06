c50097477fcd960187e1fa5cc260a5233a46d162
Will <programergeek@gmail.com>
2022-05-06 17:15:00 +0200

    create template for /create_account

--- public/index.php
+++ public/index.php
@@ -3,3 +3,7 @@
 if ($_SERVER['REQUEST_URI'] === '/') {
     include_once "../templates/Index.php";
 }
+
+if ($_SERVER['REQUEST_URI'] === '/create_account') {
+    include_once "../templates/NewAccount.php";
+}
