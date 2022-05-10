c1f5c4cf1f4b497c9b607fb05d0d1ec1e583eec8
Dihutswane <dihutswane4@gmail.com>
2022-05-10 19:51:10 +0200

    Add Reported_Crimes.php

--- src/models/Reported_Crimes.php
+++ src/models/Reported_Crimes.php
@@ -21,7 +21,41 @@ class Reported_Crimes extends Db_Connect {
         return $crime;
     }
     
-    protected function getAllReportedCrimes($reportid) {
+    protected function getReportedCrimeByDate($date) {
+        $sql = "SELECT * FROM reported_crimes WHERE date = " . $date;
+        $conn = $this->connect();
+        
+        $results = $conn->query($sql);
+        
+
+        if($results){
+		    while($row = $results->fetch_array(MYSQLI_ASSOC))
+			{
+			    $crime=$row;
+			}
+        }
+        
+        return $crime;
+    }
+
+    protected function getReportedCrimeByCrime($crime_name) {
+        $sql = "SELECT * FROM reported_crimes WHERE crime_name = " . $crime_name;
+        $conn = $this->connect();
+        
+        $results = $conn->query($sql);
+        
+
+        if($results){
+		    while($row = $results->fetch_array(MYSQLI_ASSOC))
+			{
+			    $crime=$row;
+			}
+        }
+        
+        return $crime;
+    }
+
+    protected function getAllReportedCrime($reportid) {
         $sql = "SELECT * FROM reported_crimes WHERE reportid = " . $reportid;
         $conn = $this->connect();
         
@@ -39,6 +73,43 @@ class Reported_Crimes extends Db_Connect {
         
         return $crimes;
     }
-    
+
+    protected function getAllReportedCrimesByDate($date) {
+        $sql = "SELECT * FROM reported_crimes WHERE date = " . $date;
+        $conn = $this->connect();
+        
+        $results = $conn->query($sql);
+        
+
+        $crimes = array();
+
+        if($results){
+		    while($row = $results->fetch_array(MYSQLI_ASSOC))
+			{
+			    $crimes[]=$row;
+			}
+        }
+        
+        return $crimes;
+    }
+
+    protected function getAllReportedCrimesByCrime($crime_name) {
+        $sql = "SELECT * FROM reported_crimes WHERE reportid = " . $crime_name;
+        $conn = $this->connect();
+        
+        $results = $conn->query($sql);
+        
+
+        $crimes = array();
+
+        if($results){
+		    while($row = $results->fetch_array(MYSQLI_ASSOC))
+			{
+			    $crimes[]=$row;
+			}
+        }
+        
+        return $crimes;
+    }
     
 }
