2193e87332b504394aaca5c092cc8aa034f6900d
Will <programergeek@gmail.com>
2022-05-10 09:57:43 +0200

    salt and hash the password

--- modules/SignupFormValidator.php
+++ modules/SignupFormValidator.php
@@ -1,21 +1,13 @@
 <?php
 
 if (isset($_POST['submit'])) {
+    // sanitize user input
     $first_name = htmlspecialchars($_POST['first-name']);
     $last_name = htmlspecialchars($_POST['last-name']);
     $email = htmlspecialchars($_POST['email']);
     $phone_number = htmlspecialchars($_POST['phone-number']);
-    $password = htmlspecialchars($_POST['password']);
-    $profile_pic = htmlspecialchars($_POST['profile_pic']);
-    $account_type = null;
-    $address = null;
-    $officer_id = null;
+    $password = md5("th1s15Sparta@" . htmlspecialchars($_POST['password'])); // salt and hash password
+    $address = htmlspecialchars($_POST['address']);
 
-    if ($_POST['resident'] === "on") {
-        $account_type = "resident";
-        $address = $_POST['address'];
-    } elseif ($_POST['officer'] === "on") {
-        $account_type = "officer";
-        $officer_id = $_POST['officer-id'];
-    }
+    // call controller function
 }
