a9d9a4be8c4cbee220e3e272efaaa8707fdd0a5b
Will <programergeek@gmail.com>
2022-05-11 22:39:23 +0200

    create dummy data

---
+++ public/js/dummyData.js
@@ -0,0 +1,57 @@
+export const dummy_data = {
+    0 : {
+        crime_name:"Theft",
+        area: "Block 8",
+        snippet: "awfeawefawefaw. wefawefawf awefa.",
+        date: "22/05/2022",
+        time:"15:15",
+        description: "fwwefjawe faweifjawp efiajw. eAfj faeofia wef aw aw. awef awe wefawe fawe fwe fawef awef awef. AS dawoefnawefawiefawef aew fawef awef awef awefae.",
+        long: "25.906451",
+        lat: "-24.653666",
+        id: 0
+    },
+    1 : {
+        crime_name:"Rape",
+        area: "Phase 2",
+        snippet: "awefaw efaw awef awef awef ",
+        date: "15/05/2022",
+        time:"16:23",
+        description: "awfwefawefaw ef awjefoiawjef awefj aoweifawoiefj aowiefjawoefpaijef fawejfiawef nawepf awepfawefpawefj awefja.",
+        long: "25.906451",
+        lat: "-24.653498",
+        id: 1
+    },
+    2 : {
+        crime_name:"Drug use",
+        area: "Block 7",
+        snippet: "faweifaj wefaw ef wef",
+        date: "15/04/2022",
+        time:"04:55",
+        description: "dfajwefaw peawefj apwefija pweoifjap weoifja weifja pwoeifjaw oefaw eknfawef awefieijf apwoief paowefnp awoenf apwoeifjawiej fawoiefj apfoeij aweifja.",
+        long: "25.906165",
+        lat: "-24.653486",
+        id: 2
+    },
+    3 : {
+        crime_name:"Prostitution",
+        area: "Block 6",
+        snippet: "fawefawef awef awef awef aef a",
+        date: "09/01/2022",
+        time:"21:46",
+        description: "afweo fawefjaw oefawoef awefna wejfn awleifjawefiuawnfealweifn aweiufan welfawef afe aw ieuf awefljakwef naliuwef naleif alieufhlaiweufhaliuwef alwiefnawefluiahwelf lihaef aleihf a.",
+        long: "25.906462",
+        lat: "-24.653168",
+        id: 3
+    },
+    4 : {
+        crime_name:"Robbery",
+        area: "Block 5",
+        snippet: "fawe awe fawef awef aw uhoauief hoawuefh",
+        date: "23/05/2022",
+        time:"05:13",
+        description: "fajoweifaj wefa weofajwfpawef weofiuahowiuefa dba weif awiouawehfa lkdal oiu nlaiuh lnk lui hoih oliuh ouhp phuuh uihp uhpj opih pijihjoij oij pj pj pip h.",
+        long: "25.906485",
+        lat: "-24.653216",
+        id: 4
+    }
+}
