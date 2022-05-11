2ace361c7c0337c638c3956fdf8889ebc8b2138b
Will <programergeek@gmail.com>
2022-05-11 22:58:14 +0200

    define initial state of data

--- public/js/report.js
+++ public/js/report.js
@@ -5,6 +5,12 @@ const date_time = document.getElementById("date-time")
 const description = document.getElementById("description")
 const cards = document.querySelectorAll("div.card")
 
+// initial state
+cards[0].className = `${cards[0].className} focus`
+title.textContent = dummy_data[0].crime_name + " in " + dummy_data[0].area
+date_time.textContent = `${dummy_data[0].date} • ${dummy_data[0].time}`
+description.textContent = dummy_data[0].description
+
 cards.forEach((card, i) => {
     card.addEventListener("click", () => {
         var temp = "card center"
