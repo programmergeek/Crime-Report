5f804ab73b0941aa716c2959f9a814bb84efaba8
Dihutswane <dihutswane4@gmail.com>
2022-05-13 05:42:43 +0200

    Add comments

--- src/Report_Controller.php
+++ src/Report_Controller.php
@@ -5,6 +5,7 @@ include 'Reported_Crimes.php';
 //Crime Report controller
 class Report_Controller extends Reported_Crimes {
 
+	//Invokes the addReportedCrime() function to add a reported crime to the database while also generating a report id.
 	protected function createReport($crime_name, $userid, $neighbourhood, $date, $time, $crime_description, $latitude, $longitude){
 		
 		$reportid = rand(0,1000000);
@@ -12,24 +13,27 @@ class Report_Controller extends Reported_Crimes {
 		
 	}
 	
+	//Returns all crime.
 	protected function getAllCrime() {
 	
 		$result = $this->getAllReportedCrime();
-		print_r($result);
+		return $result;
 	
 	}
 	
+	//Returns all crime of a certain nature.
 	protected function filterReportsByCrimeName($crime_name) {
 	
 		$result = $this->getAllReportedCrimeByCrime($crime_name);
-		print_r($result);
+		return $result;
 	
 	}
 	
+	//Returns all crime reported on a specific date.
 	protected function filterReportsByDate($date) {
 	
 		$result = $this->getAllReportedCrimeByDate($date);
-		print_r($result);
+		return $result;
 	
 	}
 }
